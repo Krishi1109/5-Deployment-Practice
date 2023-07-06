e8f7edc4f205d0122f4663d6f8aae9575498c869
Krishi Tanna <tannakrishi1109@gmail.com>
2023-07-06 15:11:01 +0530

    Done

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express')
-
+const path = require("path");
 const app = express()
 
 const port = 9000 
 
-app.use("/", (req,res) => {
-    res.json({Message : "Helloo World"})
-})
+
+__dirname = path.resolve();
+// if (process.env.NODE_ENV == "production") {
+if ("production"== "production") {
+console.log("Hello")
+  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", 'index.html'))
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.send("Done for the day");
+  });
+}
 
 app.listen(port, () => {
     console.log("Starting on port " + port)
